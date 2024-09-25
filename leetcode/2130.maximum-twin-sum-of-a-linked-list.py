6c4d87535600941943c08c6ed1b91152ff1c255f
Hiro Funatsuka <hiro.funatsuka@gmail.com>
2024-09-25 10:32:14 +0100

    Solve leetcode (medium, linked list, two pointers, stack)

--- leetcode/2130.maximum-twin-sum-of-a-linked-list.py
+++ leetcode/2130.maximum-twin-sum-of-a-linked-list.py
@@ -12,16 +12,37 @@
 #         self.next = next
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
-        ht = list()
-        curr = head
-        ans = 0
-        index = 0
-        while curr:
-            ht.append(curr.val)
-            index += 1
-            curr = curr.next
-        for i in range(int(len(ht)/2)):
-            ans = max(ans, ht[i] + ht[len(ht) - 1 - i])
-        return ans
+        # ht = list()
+        # curr = head
+        # ans = 0
+        # index = 0
+        # while curr:
+        #     ht.append(curr.val)
+        #     index += 1
+        #     curr = curr.next
+        # for i in range(int(len(ht)/2)):
+        #     ans = max(ans, ht[i] + ht[len(ht) - 1 - i])
+        # return ans
+
+        # https://youtu.be/doj95MelfSA?si=lKkB1Pwo7MW7zu-y
+        slow, fast = head, head
+        prev = None
+
+        while fast and fast.next:
+            fast = fast.next.next
+
+            tmp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = tmp
+
+        res = 0
+        while slow:
+            res = max(res, prev.val + slow.val)
+            prev = prev.next
+            slow = slow.next
+
+        return res
+
 
 # @lc code=end
