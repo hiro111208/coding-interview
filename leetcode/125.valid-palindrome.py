fc069a52f1db31ba02c8ec998d04e43d0d3e8b6b
Hiro Funatsuka <hiro.funatsuka@gmail.com>
2024-09-28 19:21:10 +0100

    Solve leetcode (easy, two pointers, string)

---
+++ leetcode/125.valid-palindrome.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=125 lang=python3
+#
+# [125] Valid Palindrome
+#
+
+# @lc code=start
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = ''.join(char for char in s if char.isalnum()).lower()
+        l, r = 0, len(s) - 1
+        while l < r:
+            if s[l] != s[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
+# @lc code=end
